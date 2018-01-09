40c1122aaa4dd245e9c05a6bc1efa5a62f250d3e
Mihir <mihirp38661@gmail.com>
2018-01-08 21:15:08 -0500

    add Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,21 @@
+/*
+ * Name : Mihir Patel
+ * Date : 1/28/18
+ * Desc : Calculate the sum of all the multiples of 3 or 5 below 1000.
+ */
+public class Multiples {
+	public static void main (String [] args) {
+		
+		// running total 
+		int sum = 0;
+		
+		for (int i = 3; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+		}
+		
+		System.out.println(sum);
+	}
+
+}
