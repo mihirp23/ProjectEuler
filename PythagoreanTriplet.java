21d2141ddbc4c2d7fc72862f62ca2c3e9fd9022c
mihirp23 <mihirp38661@gmail.com>
2018-01-15 11:51:16 -0500

    Update PythagoreanTriplet.java

--- PythagoreanTriplet.java
+++ PythagoreanTriplet.java
@@ -1,27 +1,32 @@
+/*
+ * Author : Mihir Patel
+ *   Date : 1/15/18
+ *   Desc : Determine the Pythagorean triplet for which a + b + c 
+ *          is equal to the specified TARGET. Pythagorean triplet
+ *          is a set of three natural numbers a < b < c for
+ *          which (a^2) + (b^2) = (c^2).
+ */
 public class PythagoreanTriplet
 {
+	/* -------------------------- main() ------------------------------ */
     public static void main (String [] args)
     {
-        final long TARGET = 25;
-        int a = 1;
-        boolean isTargetFound = false;
-        while (! isTargetFound)
+    	final int TARGET = 1000;
+        for (int a = 1; a < TARGET; a++)
         {
-            int b = a + 1;
-            while (true)
-            {
-                int c = (a * a) + (b * b);
-                System.out.println(a);
-                System.out.println(b);
-                System.out.println(c);
-                if (Math.sqrt(c) > b && b > a && (a + b + c) == TARGET)
-                {
-                    isTargetFound = true;
-                    break;
-                }
-                b++;
-            }
-            a++;
+        	for (int b = a; b < TARGET - a; b++)
+        	{
+        		for (int c = b; c < TARGET - b; c++)
+        		{
+        			if (a + b + c == TARGET && ((a * a) + (b * b) == (c *c)))
+        			{
+        				System.out.println(a);
+        				System.out.println(b);
+        				System.out.println(c);
+        			}
+        		}
+        	}
         }
+        
     }
 }
