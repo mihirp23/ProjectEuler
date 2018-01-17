3dc4302d2cf1737584b9cbfd577bbcea9668ae4d
mihirp23 <mihirp38661@gmail.com>
2018-01-17 10:21:06 -0500

    add HighlyDivisibleTriangularNumber.java

---
+++ HighlyDivisibleTriangularNumber.java
@@ -0,0 +1,43 @@
+/*
+ * Author : Mihir Patel
+ *   Date : 1/17/18
+ *   Desc : Determine the lowest TriangularNumber with TARGET divisors.
+ */
+public class HighlyDivisibleTriangularNumber 
+{
+	/* ------------------------ main() ------------------------- */
+    public static void main (String [] args)
+    {
+    	final int TARGET_DIVS = 500;
+    	long n = 1;
+    	while(true)
+    	{
+    		long triangularNumber = (n * (n + 1)) / 2;
+    		if (numOfDivisors(triangularNumber) > TARGET_DIVS)
+    		{
+    			System.out.println(triangularNumber);
+    			break;
+    		}
+    		n++;
+    	}
+    	
+    }
+    
+    /* ----------------- numOfDivisors() ----------------------- */
+    public static long numOfDivisors(long num)
+    {
+    	// brute force method
+    	int sqrt = (int)Math.sqrt(num);
+    	long divisors = 0;
+    	for (long i = 1; i < sqrt; i++)
+    	{
+    		if (num % i == 0)
+    		{
+    			// increment by 2 because we have two divisors i and (num / i)
+    			divisors += 2;
+    		}
+    	}
+    	
+    	return divisors;
+    }
+}
